a0351484a97666eec9499948f4bf286da9524a37
Osborne <osborne.mordred@gmail.com>
2019-04-23 11:27:22 +0000

    file extensions modified

--- application/helpers/file_restriction_helper.php
+++ application/helpers/file_restriction_helper.php
@@ -69,8 +69,7 @@
     if(!empty($file_array))
     {
       # File Upload Restrictions
-      $supported_extension = array('doc','docx','pdf','xls','xlsx');
-      
+      $supported_extension = array('doc','docx','pdf','xls','xlsx','txt','zip');
       $extension = strtolower(pathinfo($file_array['name'], PATHINFO_EXTENSION));
 
       if( in_array($extension,$supported_extension) ) 
